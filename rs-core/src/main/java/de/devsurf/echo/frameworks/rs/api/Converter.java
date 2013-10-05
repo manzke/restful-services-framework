f18fe7b2ceb9c7666081246ec132a4f402acbc9c
Daniel Manzke <daniel.manzke@googlemail.com>
2013-10-05 19:07:12 +0200

    introduced a marker annotation for info converter
    InfoConverter are converters, which are only mapping the necessary part
    and not all available stuff

--- rs-core/src/main/java/de/devsurf/echo/frameworks/rs/api/Converter.java
+++ rs-core/src/main/java/de/devsurf/echo/frameworks/rs/api/Converter.java
@@ -1,5 +1,20 @@
 package de.devsurf.echo.frameworks.rs.api;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.inject.Qualifier;
+
+
 public interface Converter<From, To> {
 	To convertTo(From source);
+	
+	@Target({ElementType.FIELD, ElementType.TYPE})
+	@Retention(RetentionPolicy.RUNTIME)
+	@Qualifier
+	public static @interface InfoConverter {
+		
+	}
 }
